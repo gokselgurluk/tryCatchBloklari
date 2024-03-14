3361576a32fdf64ed647c5c2b404c8f7658f6f8c
göksel gürlük <gurlukgoksel@gmail.com>
2024-03-14 16:53:18 +0300

    hata donduren metot eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 public class Main {
+
+    public  static void checkArray(int deger,int arrays[])throws ArrayIndexOutOfBoundsException{
+
+        int array = arrays[deger];
+        System.out.println("İndeksteki eleman: " + array);
+    }
+
+
+
     public static void main(String[] args) {
 
     Scanner input = new Scanner(System.in);
         int [] array ={0,1,2,3,4,5,6,7,8,9};
 
-
+        System.out.println("indeks no girin");
         int userIndexNum = input.nextInt();
         try {
-            int deger =array[userIndexNum];
-            System.out.println("İndeksteki eleman: " + deger);
+          checkArray(userIndexNum,array);
 
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("9 dan buyuk rakam giremezsiniz");
